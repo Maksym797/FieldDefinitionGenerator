1db3a09023dcb97270c8de5bdb500c96ea9bc562
Maksym M <maksym_misiuchenko@epam.com>
2021-12-20 11:28:52 +0000

    Fix

--- FieldDefinitionGenerator.Logic/CodeGenerator.cs
+++ FieldDefinitionGenerator.Logic/CodeGenerator.cs
@@ -5,8 +5,8 @@ namespace FieldDefinitionGenerator.CrmPlugin
 {
     public class CodeGenerator: IPlugin
     {
-        [Output("result")]
-        public OutArgument<string> Result
+        //[Output("result")]
+        //public OutArgument<string> Result
 
         public void Execute(IServiceProvider serviceProvider)
         {
